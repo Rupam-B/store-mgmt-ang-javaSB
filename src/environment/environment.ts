c8536226b89431cf49033e4f2bc92a508336aae0
Rupam-b <rupam.banerje@gmail.com>
2024-07-26 16:25:32 +0530

    backend functions added due to which some modifications done in routes2

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,12 +1,12 @@
 // Production base URL
-// export const environment = {
-//     production: true,
-//     baseUrl: 'https://store-mgmt-javasb-bcknd-production.up.railway.app'  
-// };
+export const environment = {
+    production: true,
+    baseUrl: 'https://store-mgmt-javasb-bcknd-production.up.railway.app'  
+};
 
 // Development base URL
-export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:8080'  
-  };
+// export const environment = {
+//     production: false,
+//     baseUrl: 'http://localhost:8080'  
+//   };
   
